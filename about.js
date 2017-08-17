f546cb11ce3326f9b55aeb4262475f3422c4d0a5
NickyLogan <laurentiusdominick@yahoo.com>
2017-08-17 22:58:18 +0700

    Responsive layout for about and the footer
    Added responsive layout for committed files.

--- about.js
+++ about.js
@@ -6,27 +6,6 @@ window.requestAnimationFrame = window.requestAnimationFrame ||
         setTimeout(f, 1000 / 60)
     }
 $(document).ready(function () {
-    var options = {
-        particleColor: '#555',
-        background: '#fff',
-        interactive: true,
-        speed: 'medium',
-        density: 9e3
-    }
-    var particleCanvas = new ParticleNetwork(document.getElementById('network'), options);
-    $('#top').mousemove(function (e) {
-        var centerX = $('#top').width() / 2;
-        var centerY = $('#top').height() / 2;
-        var offset = $('#top').offset();
-        var posX = e.pageX - offset.left;
-        var posY = e.pageY - offset.top;
-        var strength = 100;
-        // console.log('translate(' + (centerX - posX) + 'px, ' + (centerY - posY) + 'px)');
-        $('#motto').css('transform', 'translate(' + -(centerX - posX) / strength + 'px, ' + -(
-            centerY - posY) / strength + 'px)');
-        $('#network canvas').css('transform', 'translate(' + (centerX - posX) / (strength / 2) + 'px, ' + (
-            centerY - posY) / (strength / 2) + 'px)');
-    });
     var index = 0;
     checkDisable();
     var length = $('.slides>.slide').length;
@@ -39,10 +18,9 @@ $(document).ready(function () {
     }
     $('.down').click(function (e) {
         if (!$(this).hasClass('disabled')) {
-            $('.slides').css('top', '-' + (++index * 500) + 'px');
+            $('.slides').css('top', '-' + (++index * $('#vis-mis').height()) + 'px');
             $('.background').css('background-image', 'url(img/vismis' + index + '.jpeg)');
             checkDisable();
-
         }
     });
     $('.up').click(function (e) {
@@ -57,9 +35,10 @@ $(document).ready(function () {
         var scrolltop = window.pageYOffset;
         $('.background').css("background-position", "50% " + -scrolltop * 0.25 + "px");
     }
-    parallax();
-    window.addEventListener("scroll", function () {
-        requestAnimationFrame(parallax);
-    }, false);
-
+    if ($(window).width() > 992) {
+        parallax();
+        window.addEventListener("scroll", function () {
+            requestAnimationFrame(parallax);
+        }, false);
+    }
 });
