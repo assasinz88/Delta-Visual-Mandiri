7e1f73cafa3e451a02c58aa3965d67e599b74a49
Mailman <albertantonio88@gmail.com>
2017-08-21 21:33:05 +0700

    adfdfass

--- connect.php
+++ connect.php
@@ -1,16 +1,10 @@
 <?php
 $servername="localhost";
-<<<<<<< HEAD
 $db_name = "dvm";
 $user="root";
 $pwd="";
 // $user="dvms2455_admin";
 // $pwd="T7r?2ID&Qwsz";
-=======
-$db_name = "dvms2455_dvm";
-$user="dvms2455_admin";
-$pwd="T7r?2ID&Qwsz";
->>>>>>> origin/master
 
 // $db_name="dvm";
 // $user="root";
