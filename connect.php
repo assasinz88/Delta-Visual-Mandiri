31ff98de62409941579d0f79e234628adbff61e6
Mailman <albertantonio88@gmail.com>
2017-08-21 19:49:03 +0700

    1321321

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 $servername="localhost";
-// $db_name = "dvms2455_dvm";
-// $user="dvms2455_admin";
-// $pwd="T7r?2ID&Qwsz";
+$db_name = "dvms2455_dvm";
+$user="dvms2455_admin";
+$pwd="T7r?2ID&Qwsz";
 
-$db_name="dvm";
-$user="root";
-$pwd="";
+// $db_name="dvm";
+// $user="root";
+// $pwd="";
 
 $con=mysqli_connect($servername,$user,$pwd,$db_name);
 mysqli_select_db($con,"dvm");
